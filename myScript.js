b0b807c2975f43908bde8057f34491655439dfcb
Iggy <igoririanto@rocketmail.com>
2018-12-04 14:04:58 -0600

    WIP - TODO: dont trigger action when user clicks, TODO2: figure out better key naming convention

--- myScript.js
+++ myScript.js
@@ -1,11 +1,25 @@
 let counter = 0;
 
+const getSelection = () => {
+    let selectedText = ''
+    if(document.getSelection()){
+        selectedText = document.getSelection().toString()
+    }
+    return selectedText
+}
+
 document.addEventListener('mouseup', () => {
-    const selection = document.getSelection().toString()
+    // const selection = document.getSelection().toString()
+    const textSelection = getSelection()
     let zeAwesomeKey = 'zeAwesomeKey' + counter   
-    chrome.storage.sync.set({zeAwesomeKey: selection}, () => {
+    chrome.storage.sync.get(null, (results) => {
+        const allKeys = Object.keys(results)
+        console.log(allKeys.toString())
+    })
+
+    chrome.storage.sync.set({[zeAwesomeKey]: textSelection}, () => {
         counter = counter + 1
-        console.log('zeAwesome is set to ' + selection)
+        console.log('zeAwesome is set to ' + textSelection)
         console.log('Oh, counter is: ' + counter)
     })
 })
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 const texti = document.getElementById("texti")
 const buttoni = document.getElementById("buttoni")
-
+const clear_button = document.getElementById("clear_button")
 // buttoni.addEventListener("click", () => {
 //     chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT},(tab) => {
 //         chrome.tabs.sendMessage(tab[0].id, {method: 'getSelection'}, (resp) => {
@@ -13,6 +13,18 @@ const buttoni = document.getElementById("buttoni")
 buttoni.addEventListener("click", () => {
     chrome.storage.sync.get(null, (results) => {
         const allKeys = Object.keys(results)
-        texti.innerHTML = allKeys.toString
+        // texti.innerHTML = allKeys.toString()
+        texti.innerHTML = JSON.stringify(results)
+    })
+})
+
+clear_button.addEventListener("click", () => {
+    chrome.storage.sync.clear(() => {
+        const error = chrome.runtime.lastError
+        if(error) {
+            console.log(error)
+        } else {
+            texti.innerHTML = ''
+        }
     })
 })
