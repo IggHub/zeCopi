656b8e82c2d826fcd5b3218571bdd7f8a4ae47a8
Iggy <igoririanto@rocketmail.com>
2018-12-05 18:30:49 -0600

    WIP - need to be able to sync and remove from storage

--- popup.js
+++ popup.js
@@ -1,20 +1,35 @@
-const texti = document.getElementById("texti")
 const buttoni = document.getElementById("buttoni")
 const clear_button = document.getElementById("clear_button")
-// buttoni.addEventListener("click", () => {
-//     chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT},(tab) => {
-//         chrome.tabs.sendMessage(tab[0].id, {method: 'getSelection'}, (resp) => {
-//             texti.innerHTML = resp.data
-//         })
-//     }) 
-// })
+const list_display = document.getElementById("list_display")
 
+const removeElement = (elementId) => {
+    const elem = document.getElementById(elementId)
+    elem.parentNode.removeChild(elem)
+    alert("removed " + elementId)
+    return false
+}
 
 buttoni.addEventListener("click", () => {
     chrome.storage.sync.get(null, (results) => {
         const allKeys = Object.keys(results)
         // texti.innerHTML = allKeys.toString()
-        texti.innerHTML = JSON.stringify(results)
+        for(let key in results) {
+            const listItem = document.createElement("li")
+            const deleteButton = document.createElement("button")
+
+            deleteButton.className = "delete_button"
+            deleteButton.textContent = "Delete"
+            deleteButton.style.margin = "10px"
+            deleteButton.setAttribute("id", key)
+            deleteButton.addEventListener("click", () => {
+                chrome.storage.sync.remove(key, removeElement(key))
+            })
+
+            listItem.textContent = results[key]
+            listItem.appendChild(deleteButton)
+            list_display.appendChild(listItem)
+        }
+        // list_display.innerHTML = JSON.stringify(results)
     })
 })
 
@@ -24,7 +39,7 @@ clear_button.addEventListener("click", () => {
         if(error) {
             console.log(error)
         } else {
-            texti.innerHTML = ''
+            list_display.innerHTML = ''
         }
     })
 })
