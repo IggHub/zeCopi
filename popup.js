8ecb29b2f1e82c179d0df82cfbe9ef9f90307f1c
Iggy <igoririanto@rocketmail.com>
2018-12-18 13:52:43 -0600

    can copy the whole thing - DELETE gets copied though, need to get rid of that

--- popup.js
+++ popup.js
@@ -1,6 +1,41 @@
 const buttoni = document.getElementById("button-get-value")
 const clear_button = document.getElementById("clear_button")
 const list_display = document.getElementById("list_display")
+const copy_button = document.getElementById("copy_button")
+
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(
+        function() {
+            console.log("Async: Copying to clipboard was successful!");
+        },
+        function(err) {
+            console.error("Async: Could not copy text: ", err);
+        }
+    );
+}
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand("copy");
+        var msg = successful ? "successful" : "unsuccessful";
+        console.log("Fallback: Copying text command was " + msg);
+    } catch (err) {
+        console.error("Fallback: Oops, unable to copy", err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
 
 const removeElement = (elementId) => {
     const elem = document.getElementById(elementId)
@@ -13,7 +48,6 @@ const removeElement = (elementId) => {
 
 chrome.storage.sync.get(null, (results) => {
     const allKeys = Object.keys(results)
-    // texti.innerHTML = allKeys.toString()
     for(let key in results) {
         const listItem = document.createElement("li")
         const deleteButton = document.createElement("button")
@@ -31,32 +65,7 @@ chrome.storage.sync.get(null, (results) => {
         listItem.appendChild(deleteButton)
         list_display.appendChild(listItem)
     }
-    // list_display.innerHTML = JSON.stringify(results)
 })
-// buttoni.addEventListener("click", () => {
-//     chrome.storage.sync.get(null, (results) => {
-//         const allKeys = Object.keys(results)
-//         // texti.innerHTML = allKeys.toString()
-//         for(let key in results) {
-//             const listItem = document.createElement("li")
-//             const deleteButton = document.createElement("button")
-
-//             deleteButton.className = "delete_button"
-//             deleteButton.textContent = "Delete"
-//             deleteButton.style.margin = "10px"
-//             deleteButton.setAttribute("id", `${key}_delete_button`)
-//             deleteButton.addEventListener("click", () => {
-//                 chrome.storage.sync.remove(key, removeElement(key))
-//             })
-
-//             listItem.setAttribute("id", key)
-//             listItem.textContent = results[key].text
-//             listItem.appendChild(deleteButton)
-//             list_display.appendChild(listItem)
-//         }
-//         // list_display.innerHTML = JSON.stringify(results)
-//     })
-// })
 
 clear_button.addEventListener("click", () => {
     chrome.storage.sync.clear(() => {
@@ -68,3 +77,21 @@ clear_button.addEventListener("click", () => {
         }
     })
 })
+
+copy_button.addEventListener("click", () => {
+    const listCollection = list_display.getElementsByTagName("li")
+    if(listCollection.length > 0) {
+        const listArray = Array.from(listCollection)
+        const listObj = listArray.map(list =>
+                                      ({
+                                          text: list.innerText
+                                      }))
+        const textFromList = listObj.map(list => (
+            list.text
+        )).join("\r\n")
+        copyTextToClipboard(textFromList)
+    } else {
+        copyTextToClipboard('')
+    }
+})
+
